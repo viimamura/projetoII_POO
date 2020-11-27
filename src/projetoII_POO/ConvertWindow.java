f796f245afdfd603710da2f0e9440e6f104ca85a
João Pedro de Faria <51465300+JoaoFaria025@users.noreply.github.com>
2020-11-26 23:45:25 -0300

    feature:idiomasPT_EN

--- src/projetoII_POO/ConvertWindow.java
+++ src/projetoII_POO/ConvertWindow.java
@@ -10,6 +10,8 @@ import java.awt.Color;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import java.awt.Font;
+import java.util.Locale;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
@@ -21,6 +23,9 @@ public class ConvertWindow extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private static String statusIdioma;
+	private String pt = "pt";
+    private String en = "en";
 
 	/**
 	 * Launch the application.
@@ -29,6 +34,8 @@ public class ConvertWindow extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					Locale loc = Locale.getDefault();
+			        statusIdioma = loc.getLanguage();//verifica o status atual e a var recebe o mesmo
 					ConvertWindow frame = new ConvertWindow();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -42,103 +49,205 @@ public class ConvertWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public ConvertWindow() {
-		setResizable(false);
-		setTitle("ICONVERT - Convers\u00E3o de Unidades");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 718, 473);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(60, 179, 113));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBackground(Color.WHITE);
-		menuBar.setBounds(0, 0, 712, 22);
-		contentPane.add(menuBar);
-		
-		JMenu mnNewMenu = new JMenu("File");
-		mnNewMenu.setForeground(Color.BLACK);
-		mnNewMenu.setFont(new Font("Segoe UI", Font.PLAIN, 15));
-		menuBar.add(mnNewMenu);
-		
-		JMenu mnNewMenu_1 = new JMenu("Help");
-		mnNewMenu_1.setForeground(Color.BLACK);
-		mnNewMenu_1.setFont(new Font("Segoe UI", Font.PLAIN, 15));
-		menuBar.add(mnNewMenu_1);
-		
-		JLabel lblNewLabel = new JLabel("Ol\u00E1! Seja bem vindo ao ICONVERT ");
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
-		lblNewLabel.setBounds(142, 31, 427, 44);
-		contentPane.add(lblNewLabel);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		panel.setBounds(0, 104, 69, 5);
-		contentPane.add(panel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Entrada dos valores");
-		lblNewLabel_1.setForeground(Color.WHITE);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblNewLabel_1.setBounds(79, 86, 227, 32);
-		contentPane.add(lblNewLabel_1);
-		
-		textField = new JTextField();
-		textField.setBounds(89, 139, 177, 32);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(393, 139, 187, 32);
-		contentPane.add(comboBox);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("Sa\u00EDda dos valores");
-		lblNewLabel_1_1.setForeground(Color.WHITE);
-		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblNewLabel_1_1.setBounds(79, 223, 227, 32);
-		contentPane.add(lblNewLabel_1_1);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(0, 241, 69, 5);
-		contentPane.add(panel_1);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(392, 273, 177, 32);
-		contentPane.add(textField_1);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(79, 273, 187, 32);
-		contentPane.add(comboBox_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_numbers_35px.png")));
-		lblNewLabel_2.setBounds(33, 139, 46, 32);
-		contentPane.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_2_1 = new JLabel("");
-		lblNewLabel_2_1.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_export_35px.png")));
-		lblNewLabel_2_1.setBounds(33, 273, 46, 32);
-		contentPane.add(lblNewLabel_2_1);
-		
-		JLabel lblNewLabel_2_1_1 = new JLabel("");
-		lblNewLabel_2_1_1.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_currency_exchange_35px.png")));
-		lblNewLabel_2_1_1.setBounds(337, 139, 46, 32);
-		contentPane.add(lblNewLabel_2_1_1);
-		
-		JLabel lblNewLabel_2_1_1_1 = new JLabel("");
-		lblNewLabel_2_1_1_1.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_right_arrow_35px.png")));
-		lblNewLabel_2_1_1_1.setBounds(337, 273, 46, 32);
-		contentPane.add(lblNewLabel_2_1_1_1);
-		
-		JButton btnNewButton = new JButton("Converter");
-		btnNewButton.setBounds(177, 376, 89, 23);
-		contentPane.add(btnNewButton);
-		
-		JButton btnLimpar = new JButton("Limpar");
-		btnLimpar.setBounds(354, 376, 89, 23);
-		contentPane.add(btnLimpar);
+		if(statusIdioma == en) {//verifica se o idioma atual é em inglês
+			setResizable(false);
+			setTitle("ICONVERT - Convert of units");
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(100, 100, 718, 473);
+			contentPane = new JPanel();
+			contentPane.setBackground(new Color(60, 179, 113));
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			setContentPane(contentPane);
+			contentPane.setLayout(null);
+			
+			JMenuBar menuBar = new JMenuBar();
+			menuBar.setBackground(Color.WHITE);
+			menuBar.setBounds(0, 0, 712, 22);
+			contentPane.add(menuBar);
+			
+			JMenu mnNewMenu = new JMenu("FILE");
+			mnNewMenu.setForeground(Color.BLACK);
+			mnNewMenu.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+			menuBar.add(mnNewMenu);
+			
+			JMenu mnNewMenu_1 = new JMenu("AJUDA");
+			mnNewMenu_1.setForeground(Color.BLACK);
+			mnNewMenu_1.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+			menuBar.add(mnNewMenu_1);
+			
+			JLabel lblNewLabel = new JLabel("Hi! Welcome to ICONVERT");
+			lblNewLabel.setForeground(Color.WHITE);
+			lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+			lblNewLabel.setBounds(142, 31, 427, 44);
+			contentPane.add(lblNewLabel);
+			
+			JPanel panel = new JPanel();
+			panel.setBackground(Color.WHITE);
+			panel.setBounds(0, 104, 69, 5);
+			contentPane.add(panel);
+			
+			JLabel lblNewLabel_1 = new JLabel("Input values");
+			lblNewLabel_1.setForeground(Color.WHITE);
+			lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+			lblNewLabel_1.setBounds(79, 86, 227, 32);
+			contentPane.add(lblNewLabel_1);
+			
+			textField = new JTextField();
+			textField.setBounds(89, 139, 177, 32);
+			contentPane.add(textField);
+			textField.setColumns(10);
+			
+			JComboBox comboBox = new JComboBox();
+			comboBox.setBounds(393, 139, 187, 32);
+			contentPane.add(comboBox);
+			
+			JLabel lblNewLabel_1_1 = new JLabel("Output values");
+			lblNewLabel_1_1.setForeground(Color.WHITE);
+			lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+			lblNewLabel_1_1.setBounds(79, 223, 227, 32);
+			contentPane.add(lblNewLabel_1_1);
+			
+			JPanel panel_1 = new JPanel();
+			panel_1.setBackground(Color.WHITE);
+			panel_1.setBounds(0, 241, 69, 5);
+			contentPane.add(panel_1);
+			
+			textField_1 = new JTextField();
+			textField_1.setColumns(10);
+			textField_1.setBounds(392, 273, 177, 32);
+			contentPane.add(textField_1);
+			
+			JComboBox comboBox_1 = new JComboBox();
+			comboBox_1.setBounds(79, 273, 187, 32);
+			contentPane.add(comboBox_1);
+			
+			JLabel lblNewLabel_2 = new JLabel("");
+			lblNewLabel_2.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_numbers_35px.png")));
+			lblNewLabel_2.setBounds(33, 139, 46, 32);
+			contentPane.add(lblNewLabel_2);
+			
+			JLabel lblNewLabel_2_1 = new JLabel("");
+			lblNewLabel_2_1.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_export_35px.png")));
+			lblNewLabel_2_1.setBounds(33, 273, 46, 32);
+			contentPane.add(lblNewLabel_2_1);
+			
+			JLabel lblNewLabel_2_1_1 = new JLabel("");
+			lblNewLabel_2_1_1.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_currency_exchange_35px.png")));
+			lblNewLabel_2_1_1.setBounds(337, 139, 46, 32);
+			contentPane.add(lblNewLabel_2_1_1);
+			
+			JLabel lblNewLabel_2_1_1_1 = new JLabel("");
+			lblNewLabel_2_1_1_1.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_right_arrow_35px.png")));
+			lblNewLabel_2_1_1_1.setBounds(337, 273, 46, 32);
+			contentPane.add(lblNewLabel_2_1_1_1);
+			
+			JButton btnNewButton = new JButton("Convert");
+			btnNewButton.setBounds(177, 376, 89, 23);
+			contentPane.add(btnNewButton);
+			
+			JButton btnLimpar = new JButton("Clear");
+			btnLimpar.setBounds(354, 376, 89, 23);
+			contentPane.add(btnLimpar);
+		}if(statusIdioma == pt) {//verifica se o idioma atual é em português
+			setResizable(false);
+			setTitle("ICONVERT - Convers\u00E3o de Unidades");
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(100, 100, 718, 473);
+			contentPane = new JPanel();
+			contentPane.setBackground(new Color(60, 179, 113));
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			setContentPane(contentPane);
+			contentPane.setLayout(null);
+			
+			JMenuBar menuBar = new JMenuBar();
+			menuBar.setBackground(Color.WHITE);
+			menuBar.setBounds(0, 0, 712, 22);
+			contentPane.add(menuBar);
+			
+			JMenu mnNewMenu = new JMenu("FILE");
+			mnNewMenu.setForeground(Color.BLACK);
+			mnNewMenu.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+			menuBar.add(mnNewMenu);
+			
+			JMenu mnNewMenu_1 = new JMenu("AJUDA");
+			mnNewMenu_1.setForeground(Color.BLACK);
+			mnNewMenu_1.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+			menuBar.add(mnNewMenu_1);
+			
+			JLabel lblNewLabel = new JLabel("Ol\u00E1! Seja bem vindo ao ICONVERT ");
+			lblNewLabel.setForeground(Color.WHITE);
+			lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+			lblNewLabel.setBounds(142, 31, 427, 44);
+			contentPane.add(lblNewLabel);
+			
+			JPanel panel = new JPanel();
+			panel.setBackground(Color.WHITE);
+			panel.setBounds(0, 104, 69, 5);
+			contentPane.add(panel);
+			
+			JLabel lblNewLabel_1 = new JLabel("Entrada dos valores");
+			lblNewLabel_1.setForeground(Color.WHITE);
+			lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+			lblNewLabel_1.setBounds(79, 86, 227, 32);
+			contentPane.add(lblNewLabel_1);
+			
+			textField = new JTextField();
+			textField.setBounds(89, 139, 177, 32);
+			contentPane.add(textField);
+			textField.setColumns(10);
+			
+			JComboBox comboBox = new JComboBox();
+			comboBox.setBounds(393, 139, 187, 32);
+			contentPane.add(comboBox);
+			
+			JLabel lblNewLabel_1_1 = new JLabel("Sa\u00EDda dos valores");
+			lblNewLabel_1_1.setForeground(Color.WHITE);
+			lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+			lblNewLabel_1_1.setBounds(79, 223, 227, 32);
+			contentPane.add(lblNewLabel_1_1);
+			
+			JPanel panel_1 = new JPanel();
+			panel_1.setBackground(Color.WHITE);
+			panel_1.setBounds(0, 241, 69, 5);
+			contentPane.add(panel_1);
+			
+			textField_1 = new JTextField();
+			textField_1.setColumns(10);
+			textField_1.setBounds(392, 273, 177, 32);
+			contentPane.add(textField_1);
+			
+			JComboBox comboBox_1 = new JComboBox();
+			comboBox_1.setBounds(79, 273, 187, 32);
+			contentPane.add(comboBox_1);
+			
+			JLabel lblNewLabel_2 = new JLabel("");
+			lblNewLabel_2.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_numbers_35px.png")));
+			lblNewLabel_2.setBounds(33, 139, 46, 32);
+			contentPane.add(lblNewLabel_2);
+			
+			JLabel lblNewLabel_2_1 = new JLabel("");
+			lblNewLabel_2_1.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_export_35px.png")));
+			lblNewLabel_2_1.setBounds(33, 273, 46, 32);
+			contentPane.add(lblNewLabel_2_1);
+			
+			JLabel lblNewLabel_2_1_1 = new JLabel("");
+			lblNewLabel_2_1_1.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_currency_exchange_35px.png")));
+			lblNewLabel_2_1_1.setBounds(337, 139, 46, 32);
+			contentPane.add(lblNewLabel_2_1_1);
+			
+			JLabel lblNewLabel_2_1_1_1 = new JLabel("");
+			lblNewLabel_2_1_1_1.setIcon(new ImageIcon(ConvertWindow.class.getResource("/img/icons8_right_arrow_35px.png")));
+			lblNewLabel_2_1_1_1.setBounds(337, 273, 46, 32);
+			contentPane.add(lblNewLabel_2_1_1_1);
+			
+			JButton btnNewButton = new JButton("Converter");
+			btnNewButton.setBounds(177, 376, 89, 23);
+			contentPane.add(btnNewButton);
+			
+			JButton btnLimpar = new JButton("Limpar");
+			btnLimpar.setBounds(354, 376, 89, 23);
+			contentPane.add(btnLimpar);
+		}
+
 	}
 }
